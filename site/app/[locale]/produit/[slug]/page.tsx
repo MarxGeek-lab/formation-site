d29d54629370535cf1f80bc944608ca961343539
marxgeek <mgangbala610@gmail.com>
2025-09-05 04:51:54 +0100

    feat

--- site/app/[locale]/produit/[slug]/page.tsx
+++ site/app/[locale]/produit/[slug]/page.tsx
@@ -417,22 +417,16 @@ export default function ProductPage({ params }: { params: { locale: string; slug
               </Typography>
             </Box>
 
-            <Grid container spacing={4}>
+            {/* <Grid container spacing={4}>
               {relatedProducts.map((relatedProduct) => (
                 <Grid size={{ xs: 12, sm: 6, md: 4 }} key={relatedProduct.id}>
                   <ProductCard
-                    title={relatedProduct.title}
-                    category={relatedProduct.category}
-                    image={relatedProduct.image}
-                    price={relatedProduct.price}
-                    displayText={relatedProduct.displayText}
-                    features={relatedProduct.features}
-                    slug={relatedProduct.slug}
+                    product={relatedProduct}
                     locale={locale}
                   />
                 </Grid>
               ))}
-            </Grid>
+            </Grid> */}
           </Container>
         </Box>
       </Container>
