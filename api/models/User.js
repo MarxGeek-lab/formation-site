33d58bc6ab99875bac2114e1c7ab9449dc6bf779
marxgeek <mgangbala610@gmail.com>
2025-09-07 18:24:55 +0100

    feat

--- api/models/User.js
+++ api/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  password: { type: String, required: true },
+  password: { type: String },
   phoneNumber: { type: String, trim: true },
   phoneCode: { type: String, trim: true },
   gender: { type: String, trim: true },
