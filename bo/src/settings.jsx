877549aa56e82209239a697c40de26ca1a910b61
marxgeek <mgangbala610@gmail.com>
2025-12-23 20:16:59 +0100

    feat

--- bo/src/settings.jsx
+++ bo/src/settings.jsx
@@ -1,4 +1,6 @@
 
+export const API_URL = "https://api.marxgeek.com/api/";
+export const API_URL_ROOT = "https://api.marxgeek.com";
 
-export const API_URL = 'http://localhost:5000/api/';
-export const API_URL_ROOT = 'http://localhost:5000';
+// export const API_URL = 'http://localhost:5000/api/';
+// export const API_URL_ROOT = 'http://localhost:5000';
