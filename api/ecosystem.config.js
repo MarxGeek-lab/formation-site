b40e1846dfa144bae96be7cb13336a33a0f06de4
marxgeek <mgangbala610@gmail.com>
2025-12-23 19:21:28 +0100

    feat

---
+++ api/ecosystem.config.js
@@ -0,0 +1,16 @@
+// ecosystem.config.js
+module.exports = {
+    apps: [
+      {
+        name: "marxgeek-api",
+        script: "index.js",        // version compilée
+        cwd: "/var/www/marxgeek.com/api",
+        watch: false,
+        env: {
+          NODE_ENV: "production",
+          PORT: 5000,
+        },
+      },
+    ],
+  };
+  
